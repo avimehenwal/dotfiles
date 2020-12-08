709fe20f7527bbfb65cbf36b8dc661a19a2c22d5
avimehenwal <avi.mehanwal@gmail.com>
2020-12-09 00:44:29 +0100

    feat(script): 💡 image dataURL generate script

---
+++ scripts/bin/dataurl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# AUTHOR  : avimehenwal
+# CREATED : Wed Dec  9 12:32:08 AM CET 2020
+# PURPOSE : Image data url format
+
+mimetype=$(file -bN --mime-type "$1")
+content=$(base64 -w0 < "$1")
+dataurl="url('data:$mimetype;base64,$content')"
+
+echo ${dataurl}
+firefox --new-window "$dataurl"
+
+# END
