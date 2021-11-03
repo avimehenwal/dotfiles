cd16fec2e2ecc51d4b3f3d952cc9addd16124efe
avimehenwal <avi.mehanwal@gmail.com>
2021-11-03 17:38:19 +0100

    docs(packaging): rpm and deb packaging

--- shell/path.sh
+++ shell/path.sh
@@ -11,5 +11,7 @@ CARGO=$HOME/.cargo/bin
 RVM=$HOME/.rvm/bin
 MY=$HOME/dotfiles/scripts/bin
 FLUTTER=$HOME/mySoftwares/flutter/bin
+ANDROID=/home/avi/Android/Sdk/tools/bin
+DART_PUB=$HOME/.pub-cache/bin
 
-export PATH="$PATH:${LBIN}:${BREW}:${YARN}:${CARGO}:${RVM}:${MY}:${FLUTTER}"
+export PATH="$PATH:${LBIN}:${BREW}:${YARN}:${CARGO}:${RVM}:${MY}:${FLUTTER}:${ANDROID}:${DART_PUB}"
