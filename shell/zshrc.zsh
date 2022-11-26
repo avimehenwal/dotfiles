578dc28912a418727291ec206cfad3d2c1bdf42e
avimehenwal <avi.mehanwal@gmail.com>
2022-11-26 16:16:35 +0100

    chore: fedora branch

--- shell/zshrc.zsh
+++ shell/zshrc.zsh
@@ -155,3 +155,8 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/home/avi/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+# pnpm end
